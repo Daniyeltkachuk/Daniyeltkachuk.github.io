d094b78c313c5e5dffb93a21e93664348f343d32
Daniyel <Danilaradyn@mail.ru>
2020-04-20 21:22:04 +0300

    add

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -19,7 +19,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('danilaradyn7357899@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('sisoh81451@2go-mail.com');     // Add a recipient
+$mail->addAddress('hehokaj425@johnderasia.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
